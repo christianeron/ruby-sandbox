08260b466fab40399800f8af0a9fb8e55d07027e
christianeron <christianeron1@gmail.com>
2024-03-28 18:14:48 +0000

    gym 2

---
+++ sandbox.rb
@@ -0,0 +1,6 @@
+a = :GOODBYE
+if a.class == Symbol
+  pp "aa"
+else
+  pp "bb"
+end
--- think_fast.rb
+++ think_fast.rb
@@ -11,3 +11,24 @@ unpredictable_inputs = [
 
 some_random_input = unpredictable_inputs.sample
 # write your program below
+if some_random_input == nil
+  pp "no object provided"
+elsif some_random_input == true
+  pp "you may pass"
+elsif some_random_input == false
+  pp "you may not pass"
+elsif some_random_input.class == String
+  pp some_random_input.downcase
+elsif some_random_input.class == Time
+  pp some_random_input.strftime("%A").downcase
+elsif some_random_input.class == Integer
+  if some_random_input.odd?
+    pp some_random_input.to_s + " is odd"
+  else
+    pp some_random_input.to_s + " is even"
+  end 
+elsif some_random_input.class == Symbol
+  pp some_random_input.downcase
+elsif some_random_input.class == Hash
+  some_random_input.keys
+end
